8c4ad0bdf83ab97cf1fb9aa6a95a0fa1973e7363
l-yohai <yohan9612@naver.com>
2021-08-16 16:44:30 +0900

    feat: 오픈채팅방

---
+++ week_2/오픈채팅방/yohan.py
@@ -0,0 +1,19 @@
+def solution(record):
+    user_table = {}
+    answer = []
+
+    for r in record:
+        r = r.split()
+        if r[0] == 'Enter' or r[0] == 'Change':
+            # 해시테이블 초기화 {uid: nickname}
+            user_table[r[1]] = r[2]
+
+    for r in record:
+        r = r.split()
+        # 변경된 닉네임으로 메시지 생성
+        if r[0] == 'Enter':
+            answer.append(user_table[r[1]] + '님이 들어왔습니다.')
+        elif r[0] == 'Leave':
+            answer.append(user_table[r[1]] + '님이 나갔습니다.')
+
+    return answer
