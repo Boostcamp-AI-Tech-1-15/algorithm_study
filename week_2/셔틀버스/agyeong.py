398b94602ff16f0beed7d540d118f1a85ff11d81
EP000 <bzxz55@gmail.com>
2021-08-17 00:16:07 +0900

    feat: 셔틀버스

--- week_2/셔틀버스/agyeong.py
+++ week_2/셔틀버스/agyeong.py
@@ -1,27 +1,22 @@
 def solution(n, t, m, timetable):
-    
     timetable_min = sorted([int(time[:2]) * 60 + int(time[3:]) for time in timetable]) # 분 단위로 변환
+    buses = [[540+i*t, []] for i in range(n)] # 버스 타임테이블 
     
-    crew_num = len(timetable)  # 총 크루 수 			
-    bus_crew = n * m # 버스에 탈 수 있는 크루 수  		
-    
-    last_crew = timetable_min[-1] # 마지막에 타는 크루 
-    
-    # 지금 타는 차가 마지막 차라면 
-    if bus_crew - crew_num == 1: # 마지막으로 버스 탈 때 
-        if last_crew < 540: # 모두 9시 전에 나오면
-            time = 540
-        else: # 9시 이후에 나오는 크루가 있으면
-            time = last_crew - 1        
+    i = 0                                 # bus index 
+    for crew in timetable_min:
+        while (crew > buses[i][0] or      # 크루가 정류장에 버스 출발시간보다 늦게 도착하거나
+               len(buses[i][1]) == m):    # 버스가 만원이면 (while : 크루가 바로 다음버스보다 늦게 도착할 수도 있어서)
+
+            i += 1                        # 다음 버스로 넘어감
+            if i == n:                    # 마지막 버스까지 다 넘어가면 멈춤
+                break
+        else:                             # break를 만나지 않으면 
+            buses[i][1].append(crew)      # 크루를 버스에 태운다. 
+            continue                      # 아래의 break를 만나지 않게하려고 continue
         
-    elif bus_crew == crew_num: # 마지막 크루보다 일찍 나와야할 때
-        if last_crew == 1439:
-            time = 540
-        else:
-	        time = last_crew - 1
+        break                             # while의 break를 만났을때 for문도 break  
     
-    # 마지막 차가 아니라면 막차 타는 시간에 나오면 됨.
-    else:
-        time = 540 - 60 + n * t
-    
-    return '%02d:%02d' % (time//60, time%60)
+    ret = buses[-1][0] if len(buses[-1][1]) < m else buses[-1][1][-1] - 1
+    # 막차가 만원이 아니면 막차 시간에 나오고 그렇지 않으면 마지막 사람보다 1분 일찍 나온다. 
+
+    return '%02d:%02d' %(ret // 60, ret%60)  
