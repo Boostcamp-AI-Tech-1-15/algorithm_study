e57988491b537db9e01ebee1f38c3773608d9080
l-yohai <yohan9612@naver.com>
2021-08-17 16:29:11 +0900

    style: add annotation

--- week_2/오픈채팅방/yohan.py
+++ week_2/오픈채팅방/yohan.py
@@ -4,6 +4,8 @@ def solution(record):
 
     for r in record:
         r = r.split()
+        # r = [action, uid, nickname] # Enter or Change
+        # r = [action, uid] # Leave
         if r[0] == 'Enter' or r[0] == 'Change':
             # 해시테이블 초기화 {uid: nickname}
             user_table[r[1]] = r[2]
